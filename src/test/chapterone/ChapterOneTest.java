fdec12fe1b2876456f9a0b09355dc9285e733b20
anncara555@gmail.com <anncara555@gmail.com>
2022-08-23 16:04:54 +0800

    完成Junit配置

---
+++ src/test/chapterone/ChapterOneTest.java
@@ -0,0 +1,23 @@
+package chapterone;
+
+import edu.princeton.cs.algs4.StdOut;
+import org.junit.Test;
+
+//import java.util.Calendar;
+
+//import static org.junit.Assert.*;
+
+public class ChapterOneTest {
+
+    @Test
+    public void gcd() {
+        int numGcd = ChapterOne.gcd(123,321);
+        StdOut.println(numGcd);
+    }
+
+    @Test
+    public void gcd2() {
+        int numGcd = ChapterOne.gcd(123,321);
+        StdOut.println(numGcd);
+    }
+}
